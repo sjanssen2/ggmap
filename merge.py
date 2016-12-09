04852b7125efff601f7d0449447a83a88491d020
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-12-09 15:08:39 -0800

    complete for obtaining GG taxids

--- merge.py
+++ merge.py
@@ -13,18 +13,38 @@ exitFlag = 0
 
 
 class thread_fetch(threading.Thread):
-    def __init__(self, threadID, name, accessions, chunk_size):
+    def __init__(self, threadID, name, accessions, chunk_size, type_ids):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.accessions = accessions
         self.chunk_size = chunk_size
+        self.type_ids = type_ids
 
     def run(self):
         print("Starting %s with %i accessions." %
               (self.name, len(self.accessions)))
-        fetchTaxids(self.accessions, chunk_size=self.chunk_size)
+        fetchTaxids(self.accessions, chunk_size=self.chunk_size,
+                    type_ids=self.type_ids)
         print("Exiting " + self.name)
+
+
+def parse_metaphlan_markers_info(filename, abort_after_lines=20):
+    accessions = {}
+    try:
+        file = open(filename, 'r')
+        file.readline()  # header
+        readlines = 0
+        for line in file:
+            readlines += 1
+            accessions[line.rstrip().split("|")[1]] = True
+            if (abort_after_lines is not None) and \
+               (readlines >= abort_after_lines):
+                break
+        file.close()
+        return list(accessions.keys())
+    except IOError:
+        print('Cannot read file')
 
 
 def parse_gg_accessions(filename, abort_after_lines=20):
@@ -203,6 +223,46 @@ def _parse_ebi_gg(accessions):
     return results
 
 
+def _parse_img(accessions):
+    if (len(accessions) > 1):
+        print("cannot handle more than one accession at the same time!")
+        sys.exit(1)
+    else:
+        base_url = 'https://img.jgi.doe.gov/cgi-bin/m/main.cgi'
+        url = ("%s?section=TaxonDetail&page=taxonDetail&taxon_oid=%s" % (
+                base_url,
+                ",".join(accessions)))
+        response = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Fake'}))
+
+        accession = None
+        taxid = None
+        block = ""
+        results = {}
+        for line in response.read().decode('utf-8').split('\n'):
+            block += line
+            if 'http://www.ncbi.nlm.nih.gov/Taxonomy/' in line:
+                taxid = (line.split('>')[2]).split('<')[0]
+            elif "<input type='hidden' id='taxon_filter_oid' name='taxon_filter_oid' value='" in line:
+                accession = line.split("'")[7]
+            elif ("Taxon object identifier" in line) and ("not found" in line):
+                taxid = -1
+                accession = accessions[0]
+            if (accession is not None) and (taxid is not None):
+                break
+
+        if (accession is not None) and (taxid is not None):
+            try:
+                results[accession] = int(taxid)
+            except ValueError:
+                print('Parsing error (no int) for block "%s".' % block)
+                sys.exit(1)
+        else:
+            print('Parsing error (none) for block "%s".' % block)
+            sys.exit(1)
+
+        return results
+
+
 def _get_taxids_http(accessions, verbose=True, log_results=True,
                      chunk_size=100):
     if len(accessions) > 0:
@@ -235,7 +295,42 @@ def _get_taxids_http(accessions, verbose=True, log_results=True,
         return {}
 
 
-def fetchTaxids(accessions, verbose=True, log_results=True, chunk_size=100):
+def _get_taxids_img(accessions, verbose=True, log_results=True):
+    if len(accessions) > 0:
+        chunk_size = 1
+
+        if verbose:
+            print("fetching %i accessions from IMG:" % len(accessions),
+                  file=sys.stderr)
+        chunks = [accessions[i:i+chunk_size]
+                  for i in range(0, len(accessions), chunk_size)]
+
+        results = {}
+        log = None
+        for chunk_accessions in chunks:
+            if verbose:
+                print('  chunk %i: requesting "%s": ...' % (
+                        chunks.index(chunk_accessions),
+                        chunk_accessions[0]),
+                      file=sys.stderr, end="")
+            chunk_results = _parse_img(chunk_accessions)
+
+            if verbose:
+                if list(chunk_results.values())[0] == -1:
+                    print(' ID not found', file=sys.stderr)
+                else:
+                    print(' got "%s".' % (list(chunk_results.values())[0]), file=sys.stderr)
+
+            if log_results and len(chunk_results) > 0:
+                log = write_accession_taxids(chunk_results, filehandle=log)
+
+            results = {**results, **chunk_results}
+        return results
+    else:
+        return {}
+
+
+def fetchTaxids(accessions, type_ids, verbose=True, log_results=True, chunk_size=100):
     """ Fetches NCBI taxonomy IDs via EBI for a list of accessions.
     """
 
@@ -244,8 +339,14 @@ def fetchTaxids(accessions, verbose=True, log_results=True, chunk_size=100):
 
     # second, for the remaining accessions, start a REST request to EBI
     http_accessions = list(set(accessions) - set(cached_results.keys()))
-    http_results = _get_taxids_http(http_accessions, verbose, log_results,
-                                    chunk_size=chunk_size)
+    if type_ids == 'Genbank':
+        http_results = _get_taxids_http(http_accessions, verbose, log_results,
+                                        chunk_size=chunk_size)
+    elif type_ids == 'IMG':
+        http_results = _get_taxids_img(http_accessions, verbose, log_results)
+    else:
+        print("Unknown ID type.")
+        sys.exit(1)
 
     return {**cached_results, **http_results}
 
@@ -258,26 +359,39 @@ def slice_it(li, cols=2):
         start = stop
 
 
-if __name__ == "__main__":
-    abort_after_lines = 1000
-    num_threads = 1
-    inner_chunk_size = 200
-    file_input = '/home/sjanssen/GreenGenes/gg_13_5_accessions.txt'
-
-    r = parse_gg_accessions(file_input, abort_after_lines=abort_after_lines)
-    genbank_ids = [v for k, v in r['Genbank'].items()]
-    chunks = list(slice_it(genbank_ids, num_threads))
+def fetchTaxids_threaded(accessions, type_ids, num_threads):
+    chunks = list(slice_it(accessions, num_threads))
     threads = []
     for chunk in chunks:
         threads.append(thread_fetch(
             chunks.index(chunk),
             "Thread-%i" % chunks.index(chunk),
             chunk,
-            inner_chunk_size
+            inner_chunk_size,
+            type_ids
         ))
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
+
+
+if __name__ == "__main__":
+    abort_after_lines = None
+    num_threads = 5
+    inner_chunk_size = 200
+    file_input = '/home/sjanssen/GreenGenes/gg_13_5_accessions.txt'
+    runtype = 'metaphlan'
+
+    accessions = []
+    if runtype == 'GreenGenes':
+        r = parse_gg_accessions(file_input,
+                                abort_after_lines=abort_after_lines)
+        fetchTaxids_threaded([v for k, v in r['Genbank'].items()],
+                             'Genbank', num_threads)
+        fetchTaxids_threaded([v for k, v in r['IMG'].items()],
+                             'IMG', num_threads)
+    elif runtype == 'metaphlan':
+        accessions = parse_metaphlan_markers_info('/home/sjanssen/GreenGenes/Metaphlan/markers_info.txt', abort_after_lines=abort_after_lines)
 
     print("Exiting Main Thread")
