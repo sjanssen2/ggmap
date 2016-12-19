097564a0ff8278ce05a32747796f36dbdf295be0
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-12-19 09:51:07 -0800

    parse out MP marker clades

--- map_ids.py
+++ map_ids.py
@@ -19,43 +19,49 @@ def parse_gg_otu_map(filename, abort_after_lines=20):
     except IOError:
         print('Cannot read file')
 
-# def parse_metaphlan_markers_info(filename, abort_after_lines=20):
-#     clades = {}
-#     try:
-#         file = open(filename, 'r')
-#         readlines = 0
-#         for line in file:
-#             readlines += 1
-#             if line.startswith('gi|'):
-#                 type_ids = 'gi'
-#                 accession = (line.split('\t')[0]).split('|')[1]
-#             elif line.startswith('GeneID:'):
-#                 type_ids = 'GeneID'
-#                 accession = (line.split('\t')[0]).split(':')[1]
-#             elif line.startswith('NC_'):
-#                 type_ids = 'NC'
-#                 accession = line.split('\t')[0]
-#             else:
-#                 type_ids = None
-#                 accession = None
-#
-#             if (type_ids is not None) and (accession is not None):
-#                 clade = line.split("clade': '")[1].split("'")[0]
-#                 if clade not in clades:
-#                     clades[clade] = {}
-#                 if type_ids not in clades[clade]:
-#                     clades[clade][type_ids] = {}
-#                 clades[clade][type_ids][accession] = True
-#
-#             if (abort_after_lines is not None) and \
-#                (readlines >= abort_after_lines):
-#                 break
-#         file.close()
-#         return clades
-#
-#     except IOError:
-#         print('Cannot read file')
-#
+
+def parse_metaphlan_markers_info(filename, abort_after_lines=20):
+    clades = {}
+    try:
+        file = open(filename, 'r')
+        readlines = 0
+        for line in file:
+            readlines += 1
+            if line.startswith('gi|'):
+                type_ids = 'gi'
+                accession = (line.split('\t')[0]).split('|')[1]
+            elif line.startswith('GeneID:'):
+                type_ids = 'GeneID'
+                accession = (line.split('\t')[0]).split(':')[1]
+            elif line.startswith('NC_'):
+                type_ids = 'NC'
+                accession = line.split('\t')[0]
+            else:
+                type_ids = None
+                accession = None
+
+            if (type_ids is not None) and (accession is not None):
+                clade = line.split("clade': '")[1].split("'")[0]
+                if clade not in clades:
+                    clades[clade] = {}
+                if type_ids not in clades[clade]:
+                    clades[clade][type_ids] = {}
+                clades[clade][type_ids][accession] = True
+
+            if (abort_after_lines is not None) and \
+               (readlines >= abort_after_lines):
+                break
+
+        for clade in clades:
+            for type_id in clades[clade]:
+                clades[clade][type_id] = set(clades[clade][type_id].keys())
+
+        file.close()
+        return clades
+
+    except IOError:
+        print('Cannot read file')
+
 if __name__ == "__main__":
     abort_after_lines = None
     file_otumap97 = '/home/sjanssen/GreenGenes/gg_13_5_otus/otus/97_otu_map.txt'
