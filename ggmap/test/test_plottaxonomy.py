99fe74f38e9a7b24b518d6c10bc2ec436835490a
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-02-27 16:24:40 -0800

    corrected taxonomy path

--- ggmap/test/test_plottaxonomy.py
+++ ggmap/test/test_plottaxonomy.py
@@ -16,7 +16,7 @@ from ggmap.snippets import plotTaxonomy
 plt.switch_backend('Agg')
 
 
-def generate_plots(biomfile, metadata, outdir=None, extension='.png',
+def generate_plots(biomfile, metadata, taxonomy, outdir=None, extension='.png',
                    list_existing=False):
     if outdir is None:
         outdir = tempfile.mkdtemp() + "/"
@@ -29,7 +29,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
         'params': {'file_otutable': biomfile,
                    'metadata': metadata,
                    'verbose': False,
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_reorder'] = {
         'description': ('Check if samples are re-ordered according to most '
                         'abundant taxa.'),
@@ -37,14 +37,14 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'metadata': metadata,
                    'verbose': False,
                    'reorder_samples': True,
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_samplenames'] = {
         'description': 'Can we plot sample names on the X-axis?',
         'params': {'file_otutable': biomfile,
                    'metadata': metadata,
                    'verbose': False,
                    'print_sample_labels': True,
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_reorder_samplenames'] = {
         'description': ('Get the sample names on the X-axis re-ordered '
                         'properly?'),
@@ -53,7 +53,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'verbose': False,
                    'print_sample_labels': True,
                    'reorder_samples': True,
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_groupl1'] = {
         'description': 'Four l1 groups',
         'params': {'file_otutable': biomfile,
@@ -62,7 +62,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'print_sample_labels': False,
                    'reorder_samples': True,
                    'group_l1': 'diet_brief',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_groupl1_sampellables'] = {
         'description': 'Do we have sample labels, if group l1 is given?',
         'params': {'file_otutable': biomfile,
@@ -71,7 +71,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'print_sample_labels': True,
                    'reorder_samples': True,
                    'group_l1': 'diet_brief',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_groupl2'] = {
         'description': 'Can we further subdivide g1 into g2?',
         'params': {'file_otutable': biomfile,
@@ -81,7 +81,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'reorder_samples': True,
                    'group_l1': 'diet_brief',
                    'group_l2': 'genspec',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_groupl2_samplelabels'] = {
         'description': ('Do we get the clash with sample labels if l2 group '
                         'is present?'),
@@ -92,7 +92,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'reorder_samples': True,
                    'group_l1': 'diet_brief',
                    'group_l2': 'genspec',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_groupl0'] = {
         'description': 'Also sub-group into Q2 geography.',
         'params': {'file_otutable': biomfile,
@@ -103,7 +103,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'group_l1': 'diet_brief',
                    'group_l2': 'genspec',
                    'group_l0': 'Q2',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_species'] = {
         'description': 'Collapse on Species instead of Phylum',
         'params': {'file_otutable': biomfile,
@@ -115,7 +115,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'group_l2': 'genspec',
                    'group_l0': 'Q2',
                    'rank': 'Species',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_minreads'] = {
         'description': 'Stricter read threshold',
         'params': {'file_otutable': biomfile,
@@ -128,7 +128,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'group_l0': 'Q2',
                    'rank': 'Species',
                    'minreadnr': 10000,
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_onlyg1'] = {
         'description': ('Can we have vertical lines for l2, even if no l1 is'
                         ' defined?'),
@@ -138,7 +138,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'print_sample_labels': False,
                    'reorder_samples': True,
                    'group_l2': 'genspec',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_onlyg0'] = {
         'description': 'Can we have rows if no l1 or l2 is defined?',
         'params': {'file_otutable': biomfile,
@@ -147,7 +147,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'print_sample_labels': False,
                    'reorder_samples': True,
                    'group_l0': 'Q2',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
     configs['tp_nog1'] = {
         'description': 'Several rows and vertical lines, but no l1 defined.',
         'params': {'file_otutable': biomfile,
@@ -157,7 +157,7 @@ def generate_plots(biomfile, metadata, outdir=None, extension='.png',
                    'reorder_samples': True,
                    'group_l2': 'genspec',
                    'group_l0': 'Q2',
-                   'file_taxonomy': self.taxonomy}}
+                   'file_taxonomy': taxonomy}}
 
     if not list_existing:
         for name in configs:
@@ -188,11 +188,13 @@ class TaxPlotTests(TestCase):
         self.taxonomy = get_data_path('97_otu_taxonomy.txt')
         genBaseline = False
         self.plots_baseline = generate_plots(self.filename_biom, self.metadata,
+                                             self.taxonomy,
                                              outdir=self.baselinedir,
                                              list_existing=not genBaseline)
 
     def test_regression_plots(self):
-        plots = generate_plots(self.filename_biom, self.metadata)
+        plots = generate_plots(self.filename_biom, self.metadata,
+                               self.taxonomy)
 
         print("Comparing images (%i): " % len(plots), file=sys.stderr, end="")
         for name in plots:
