cd283b1291aa58d355b9fa8c9b807bde51cc6eba
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-08-04 11:22:56 -0700

    more tests for cluster_run

--- ggmap/test/test_qsub.py
+++ ggmap/test/test_qsub.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, main
+from io import StringIO
 
 from skbio.util import get_data_path
 
@@ -32,6 +33,42 @@ class QsubTests(TestCase):
     def test_cluster_run(self):
         res = cluster_run([], "jobname", get_data_path("float.tsv"))
         self.assertEqual(res, 'Result already present!')
+
+    def test_cluster_run_slurm(self):
+        exp = """#!/bin/bash
+
+#SBATCH --job-name=cr_jobname
+#SBATCH --output=/home/sjanssen/GreenGenes/ggmap/%A.log
+#SBATCH --partition=hii02
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --time=0-4:0
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=sjanssen@ucsd.edu
+
+srun uname -a
+srun find /tmp/ -name *.png
+
+
+"""
+        out = StringIO()
+        cluster_run("find /tmp/ -name *.png", "jobname",
+                    "/tmp/teststxxx", slurm=True, out=out)
+        self.assertEqual(out.getvalue(), exp)
+
+    def test_cluster_run_highmem(self):
+        out = StringIO()
+        cluster_run("find /tmp/ -name *.png", "jobname",
+                    "/tmp/teststxxx", out=out, pmem='1000GB')
+        self.assertIn(':highmem:', out.getvalue())
+
+    def test_cluster_run_env(self):
+        out = StringIO()
+        cluster_run("find /tmp/ -name *.png", "jobname",
+                    "/tmp/teststxxx", out=out, pmem='1000GB',
+                    environment="qiime_env")
+        self.assertIn('source activate qiime_env && ', out.getvalue())
 
 
 if __name__ == '__main__':
