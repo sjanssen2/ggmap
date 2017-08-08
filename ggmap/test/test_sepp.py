f635ad943e742c221bb6efccf38d9aa45c1d7617
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-08-08 16:28:25 -0700

    new tests for load pynast

--- ggmap/test/test_sepp.py
+++ ggmap/test/test_sepp.py
@@ -1,16 +1,59 @@
 from unittest import TestCase, main
+from io import StringIO
+from tempfile import mkstemp
+from os import remove
 
 from skbio.util import get_data_path
 
-from ggmap.sepp import (read_otumap)
+from ggmap.sepp import (read_otumap, load_sequences_pynast)
 
 
 class SeppTests(TestCase):
+    def setUp(self):
+        self.file_pynast = get_data_path('gg_13_5_pynast.fasta')
+        self.file_otumap = get_data_path('61_otu_map.txt')
+        self.exp_fragments = [
+            {'sequence':
+             ('TACGAAGGGACCTAGCGTAGTTCGGAATTACTGGGCTTAAAGAGTTCGTAGGTGGTTAAAAAA'
+              'GTTGGTGGTGAAAGCCCAGAGCTTAACTCTGGAACGGCCATCAAAACTTTTTAGCTAGAGTAT'
+              'GATAGAGGAAAGCAGAATTTCTAG'),
+             'OTUID': '4336814'},
+            {'sequence':
+             ('TACGGAGGTGGCACGCGTTGTCCGGAATTATTGGGCGTAAAGCGCGCGGGTGCCCGCACTAGC'
+              'TGGAGTGGGCGTATAGCTCGCGATGGTGGTTCCTTAAGTGTGATGTGAAAAGCTCCCGGCTCA'
+              'ACCGGGGAGAGTCATTGGAACTGG'),
+             'OTUID': '734152'},
+            {'sequence':
+             ('AACCAGCTCTTCAAGTGGTCGGGATAATTATTGGGCTTAAAGTGTCCGTAGCTTGTATAATAA'
+              'GTTCCTGGTAAAATCTAATAGCTTAACTATNAGTATGCTAGGAATACTGTTGTACTAGAGGGC'
+              'GGGAGAGGTCTGAGGTACTTCAGG'),
+             'OTUID': '1928988'},
+            {'sequence':
+             ('GACATAGGTCGCGAACGTTATCCGGAATTATTGGGCGTAAAGGATGCGTAGATGGTTCAGTAA'
+              'GTTACTGGTGGGAAATCGAGGCCTAACCTCGTGGAAGTCAGTAATACTGTTGAACTTGAGTGC'
+              'AGGAGAGGTTAACGGAACTTCATG'),
+             'OTUID': '696036'},
+            {'sequence':
+             ('TACGGAGGGGGCAAGCGTTGTCGGAATAACTGGGCCTAAAGCCGCGCCGTAGGCGGGTTTGTT'
+              'AAGTCAGATGTGAAAGCCCTCGGCTCAACCGGGGACGTGGCATTTGAACTGGCCAACTTGAGT'
+              'ACTGGAGGGGGGGGGAATCCCGTG'),
+             'OTUID': '4459468'},
+            {'sequence':
+             ('CACCGGCGGCCCGAGTGGTGACCGTTATTATTGGGTCTAAAGGGTCCGTAGCCGGTTTGGTCA'
+              'GTCCTCCGGGAAATCTGATAGCTTAACTGTTAGGCTTTCGGGGGATACTGCCAGGCTTGGAAC'
+              'CGGGAGAGGTAAGAGGTACTACAG'),
+             'OTUID': '1128285'},
+            {'sequence':
+             ('GAACCTCGGCTCGAGTGGTGGCCGCTTTTATTGGGCTTAAAGCGTTCGTAGCTGGGTTGTTAA'
+              'GTCTCTTGGGAAATCTGGCGGCTTAACCGTCAGGCGTCTAAGGGATACTGGCAATCTTGGAAC'
+              'CGGGAGAGGTGAGGGGTACTTCGG'),
+             'OTUID': '4455990'}]
+
     def test_read_otumap(self):
         with self.assertRaisesRegex(IOError, "Cannot read file"):
             read_otumap('/dev/aucguacguguac')
 
-        obs = read_otumap(get_data_path('61_otu_map.txt'))
+        obs = read_otumap(self.file_otumap)
         self.assertEqual(obs.shape, (22, ))
         self.assertEqual(obs.index.name, 'representative')
         self.assertEqual(obs.name, 'non-representatives')
@@ -19,6 +62,55 @@ class SeppTests(TestCase):
                          sorted(['2014493', '951205', '734152']))
         self.assertIn('2107103', obs.index)
         self.assertEqual(obs['2107103'], [])
+
+    def test_load_sequences_pynast(self):
+        out = StringIO()
+        obs = load_sequences_pynast(self.file_pynast,
+                                    self.file_otumap,
+                                    2263, 3794, 150, file_cache=None, out=out)
+        obs_out = out.getvalue()
+        self.assertIn("%i rows and %i cols in alignment '%s'" %
+                      (9, 7682, self.file_pynast.split('/')[-1]), obs_out)
+        self.assertIn("%i sequences in OTU map '%s'" %
+                      (33, self.file_otumap.split('/')[-1]), obs_out)
+        self.assertIn(("%i sequences selected from OTU map and alignment. "
+                       "Surprise: %i sequences of OTU map are NOT in "
+                       "alignment!") %
+                      (9, 24), obs_out)
+        self.assertIn(("%i -> %i cols: trimming alignment to fragment "
+                       "coordinates") %
+                      (7682, 1531), obs_out)
+        self.assertIn(("%i fragments with ungapped length >= %int. "
+                       "Surprise: %i fragments are too short and %i "
+                       "fragments where too long "
+                       "(and have been trimmed)!") %
+                      (7, 150, 2, 3), obs_out)
+        self.assertCountEqual(obs, self.exp_fragments)
+
+    def test_load_sequences_pynast_testcache(self):
+        out = StringIO()
+        file_dummy = mkstemp()[1]
+        remove(file_dummy)
+
+        # first run: create cache file
+        obs = load_sequences_pynast(self.file_pynast,
+                                    self.file_otumap,
+                                    2263, 3794, 150, file_cache=file_dummy,
+                                    out=out)
+        obs_out = out.getvalue()
+        self.assertIn("Stored results to cache '%s'" % file_dummy, obs_out)
+        self.assertCountEqual(obs, self.exp_fragments)
+
+        # second run: load cached results
+        out2 = StringIO()
+        obs = load_sequences_pynast(self.file_pynast,
+                                    self.file_otumap,
+                                    2263, 3794, 150, file_cache=file_dummy,
+                                    out=out2)
+        obs_out = out2.getvalue()
+        self.assertIn("%i fragments loaded from cache '%s'" % (7, file_dummy),
+                      obs_out)
+        self.assertCountEqual(obs, self.exp_fragments)
 
 
 if __name__ == '__main__':
