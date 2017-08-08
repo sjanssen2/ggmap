0b1f40586267f28f09d82f17ebed550bedd3f9c2
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-08-07 17:24:36 -0700

    -v for time not for uname :-/

--- ggmap/snippets.py
+++ ggmap/snippets.py
@@ -933,13 +933,13 @@ def cluster_run(cmds, jobname, result, environment=None,
         # names are
         timing_cmds = []
         # report machine name
-        timing_cmds.append('uname -v -a > ${PBS_JOBNAME}.t${PBS_JOBID}')
+        timing_cmds.append('uname -a > ${PBS_JOBNAME}.t${PBS_JOBID}')
         # report commands to be executed (I have problems with quotes)
         # timing_cmds.append('echo `%s` >> ${PBS_JOBNAME}.t${PBS_JOBID}'
         #                    % '; '.join(cmds))
         # add time to every command
         timing_cmds.extend(map(lambda cmd:
-                               '%s -o ${PBS_JOBNAME}.t${PBS_JOBID} -a %s' %
+                               '%s -v -o ${PBS_JOBNAME}.t${PBS_JOBID} -a %s' %
                                (EXEC_TIME, cmd),
                                cmds))
         cmds = timing_cmds
