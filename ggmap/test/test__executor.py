784520061b06ec1e547a8a850393df24251d5d07
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-08-20 20:14:35 -0700

    mini test for _executor

---
+++ ggmap/test/test__executor.py
@@ -0,0 +1,35 @@
+from unittest import TestCase, main
+
+from ggmap.analyses import _executor
+
+
+class ExecutorTests(TestCase):
+    def test__executor(self):
+        def pre_execute(workdir, args):
+            file_mapping = workdir + '/headermap.tsv'
+            m = open(file_mapping, 'w')
+            m.write('pre_execute test\n')
+            m.close()
+
+        def commands(workdir, ppn, args):
+            commands = ['python --version >> %s 2>&1' % (workdir+'/headermap.tsv')]
+            return commands
+
+        def post_execute(workdir, args, pre_data):
+            cont = []
+            f = open(workdir+'/headermap.tsv', 'r')
+            for line in f.readlines():
+                cont.append(line)
+            f.close()
+            return cont
+
+        res = _executor('travistest',
+                        {'seqs': 'fake'},
+                        pre_execute,
+                        commands,
+                        post_execute,
+                        dry=False, wait=True, use_grid=False, nocache=True, timing=False)
+        print("==res==:", res)
+
+if __name__ == '__main__':
+    main()
