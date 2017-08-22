a3c27c827a06de844e5482abd13c93b237be1004
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-08-22 10:54:07 -0700

    cover remaining lines for _get_ref_phylogeny

--- ggmap/test/test_analyses_helper.py
+++ ggmap/test/test_analyses_helper.py
@@ -2,7 +2,7 @@ from unittest import TestCase, main
 
 from skbio.util import get_data_path
 
-from ggmap.analyses import (_parse_alpha_div_collated)
+from ggmap.analyses import (_parse_alpha_div_collated, _get_ref_phylogeny)
 
 
 class AnalysesHelperTests(TestCase):
@@ -27,6 +27,12 @@ class AnalysesHelperTests(TestCase):
 
         with self.assertRaisesRegex(IOError, 'Cannot read file'):
             _parse_alpha_div_collated('/dev/null/test')
+
+    def test__get_ref_phylogeny(self):
+        self.assertEqual(_get_ref_phylogeny('testfile'), 'testfile')
+
+        with self.assertRaises(ValueError):
+            _get_ref_phylogeny(env='wrong')
 
 
 if __name__ == '__main__':
