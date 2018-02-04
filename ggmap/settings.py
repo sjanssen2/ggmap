297ae637e6a6489cbe67c7ee7320fa4a1707aad6
sjanssen2 <stefan.m.janssen@gmail.com>
2018-02-03 21:03:04 -0800

    added a settings file mechanism

---
+++ ggmap/settings.py
@@ -0,0 +1,54 @@
+import yaml
+import os
+import sys
+
+FP_SETTINGS = os.path.join(os.environ['HOME'], '.ggmaprc')
+
+DEFAULTS = {'condaenv_qiime1': {'default': 'qiime_env',
+                                'variable_name': 'QIIME_ENV'},
+            'condaenv_qiime2': {'default': 'qiime2-2017.10',
+                                'variable_name': 'QIIME2_ENV'},
+            'fp_reference_phylogeny': {'default': None,
+                                       'variable_name': 'FILE_REFERENCE_TREE'},
+            'fp_binary_time': {'default': '/usr/bin/time',
+                               'variable_name': 'EXEC_TIME'},
+            'list_ranks': {'default': ['Kingdom', 'Phylum', 'Class', 'Order',
+                                       'Family', 'Genus', 'Species'],
+                           'variable_name': 'RANKS'},
+            }
+
+
+# stolen from https://stackoverflow.com/questions/13034496/
+# using-global-variables-between-files
+def init(err=sys.stderr):
+    # load settings from file
+    if os.path.exists(FP_SETTINGS):
+        err.write("Reading settings file '%s'" % FP_SETTINGS)
+        with open(FP_SETTINGS, 'r') as f:
+            config = yaml.load(f)
+    else:
+        config = dict()
+
+    # if variable is not set in settings file, fall back to default
+    for field in DEFAULTS.keys():
+        if field not in config:
+            config[field] = DEFAULTS[field]['default']
+
+    # set global variables to values from config file or fall back to defaults
+    global QIIME_ENV
+    QIIME_ENV = config['condaenv_qiime1']
+    global QIIME2_ENV
+    QIIME2_ENV = config['condaenv_qiime2']
+    global FILE_REFERENCE_TREE
+    FILE_REFERENCE_TREE = config['fp_reference_phylogeny']
+    global EXEC_TIME
+    EXEC_TIME = config['fp_binary_time']
+    global RANKS
+    RANKS = config['list_ranks']
+
+    # if settings file does not exist, create one with current values as a
+    # primer for user edits
+    if not os.path.exists(FP_SETTINGS):
+        with open(FP_SETTINGS, 'w') as f:
+            yaml.dump(config, f)
+        err.write('New config file "%s" created.' % FP_SETTINGS)
---
+++ ggmap/test/test_settings.py
@@ -0,0 +1,55 @@
+from unittest import TestCase, main
+from io import StringIO
+from tempfile import mkstemp
+from os import remove
+import os
+import yaml
+
+from ggmap import settings
+
+
+class SettingsTests(TestCase):
+    def setUp(self):
+        self.file_fake_settings = mkstemp()[1]
+        self.err = StringIO()
+        remove(self.file_fake_settings)
+
+    def tearDown(self):
+        if os.path.exists(self.file_fake_settings):
+            remove(self.file_fake_settings)
+
+    def test_write_primer(self):
+        # ensure that a new configuration file is written as primer with
+        # default values if config file does not exists. This will help users
+        # to change settings.
+        settings.FP_SETTINGS = self.file_fake_settings
+        settings.init(err=self.err)
+        self.assertEqual(self.err.getvalue(), 'New config file "%s" created.' %
+                         self.file_fake_settings)
+
+    def test_defaults(self):
+        # check that value is not yet present
+        with self.assertRaises(AttributeError):
+            print(settings.RANKS)
+
+        # check that default value is used after init()
+        settings.FP_SETTINGS = self.file_fake_settings
+        settings.init(err=self.err)
+        self.assertIn('Kingdom', settings.RANKS)
+        self.assertEqual('/usr/bin/time', settings.EXEC_TIME)
+        self.assertEqual(self.err.getvalue(), 'New config file "%s" created.' %
+                         self.file_fake_settings)
+
+    def test_partial_defined(self):
+        # test that values set by file are used and non-set fall back to
+        # default
+        with open(self.file_fake_settings, 'w') as f:
+            yaml.dump({'list_ranks': ['R1', 'R2']}, f)
+        settings.FP_SETTINGS = self.file_fake_settings
+        settings.init(err=self.err)
+        self.assertEqual('/usr/bin/time', settings.EXEC_TIME)
+        self.assertEqual(['R1', 'R2'], settings.RANKS)
+
+
+if __name__ == '__main__':
+    main()
