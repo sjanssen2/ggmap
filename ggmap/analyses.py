9d7042fb0ae81f828e4b43c60c32ebc0466cba9e
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-06-24 00:39:10 -0700

    manual adding picrust/bugbase again in an attempt to merge

--- ggmap/analyses.py
+++ ggmap/analyses.py
@@ -1869,6 +1869,192 @@ def compare_categories(beta_dm, metadata,
                      **executor_args)
 
 
+def picrust(counts, **executor_args):
+    """Translate closed ref OTU tables into predicted meta-transcriptomics.
+
+    Parameters
+    ----------
+    counts : Pandas.DataFrame
+        OTU counts
+    executor_args:
+        dry, use_grid, nocache, wait, walltime, ppn, pmem, timing, verbose
+
+    Returns
+    -------
+    One hash with following 8 tables:
+    {'rfam': [c],
+     'KEGG_Pathways': [c, c, c, c],
+     'COG_Category': [c, c, c]}
+
+    Install
+    -------
+    conda create --name picrust python=2.7
+    source activate picrust
+    wget https://github.com/picrust/picrust/releases/down \
+        load/v1.1.3/picrust-1.1.3.tar.gz
+    tar xzvf picrust-1.1.3.tar.gz
+    pip install numpy
+    pip install h5py
+    pip install .
+    download_picrust_files.py -t ko
+    download_picrust_files.py -t cog
+    download_picrust_files.py -t rfam
+    """
+    TYPES = ['rfam', 'ko', 'cog']
+
+    def get_catname_levels(type_):
+        levels = None
+        category = None
+        if type_ == 'ko':
+            category = 'KEGG_Pathways'
+            levels = list(range(1, 4))
+        elif type_ == 'cog':
+            category = 'COG_Category'
+            levels = list(range(1, 3))
+        elif type_ == 'rfam':
+            category = 'Rfam'
+            levels = []
+        return category, levels
+
+    def pre_execute(workdir, args):
+        if all(map(lambda x: x.isdigit(), args['counts'].index)):
+            pandas2biom(workdir+'/input.biom', args['counts'].fillna(0.0))
+        else:
+            raise ValueError(
+                ('Not all features are numerical, that might point to the fact'
+                 ' that your count table does not stem from closed reference '
+                 'picking vs. Greengenes.'))
+
+    def commands(workdir, ppn, args):
+        commands = []
+
+        # Normalize to copy number
+        commands.append(('normalize_by_copy_number.py '
+                         '-i "%s/input.biom" '
+                         '-o "%s/normalized.biom"') %
+                        (workdir, workdir))
+
+        # PICRUSt prediction
+        for type_ in TYPES:
+            commands.append(('predict_metagenomes.py '
+                             '-t %s '
+                             '-i "%s/normalized.biom" '
+                             '-o "%s/%s"') % (
+                    type_, workdir, workdir, 'picrust_%s_coll-0.biom' % type_))
+
+        # Collapse at higher hierarchy levels
+        for type_ in TYPES:
+            category, levels = get_catname_levels(type_)
+            for level in levels:
+                commands.append((
+                    'categorize_by_function.py '
+                    '-i "%s/picrust_%s_coll-0.biom" '
+                    '-o "%s/picrust_%s_coll-%s.biom" -l %i -c %s') %
+                    (workdir, type_, workdir, type_, level, level, category))
+
+        return commands
+
+    def post_execute(workdir, args):
+        results = dict()
+        for type_ in TYPES:
+            category, levels = get_catname_levels(type_)
+            results[category] = dict()
+            for level in [0] + levels:
+                results[category][level] = biom2pandas(
+                    '%s/picrust_%s_coll-%s.biom' % (workdir, type_, level))
+        return results
+
+    return _executor('picrust',
+                     {'counts': counts},
+                     pre_execute,
+                     commands,
+                     post_execute,
+                     ppn=1,
+                     environment=settings.PICRUST_ENV,
+                     **executor_args)
+
+
+def bugbase(counts, **executor_args):
+    """BugBase is a microbiome analysis tool that determines high-level
+       phenotypes present in microbiome samples.
+
+    Parameters
+    ----------
+    counts : Pandas.DataFrame
+        OTU counts
+    executor_args:
+        dry, use_grid, nocache, wait, walltime, ppn, pmem, timing, verbose
+
+    Returns
+    -------
+    A dict of count tables, where every BugBase trait is one key-value pair,
+    i.e. currently, following 9 traits get predicted:
+      'Gram_Negative', 'Aerobic', 'Anaerobic', 'Stress_Tolerant',
+      'Forms_Biofilms', 'Facultatively_Anaerobic', 'Potentially_Pathogenic',
+      'Contains_Mobile_Elements', 'Gram_Positive'
+    Input counts get normalized as with Picrust.
+
+    Install
+    -------
+    git clone https://github.com/knights-lab/BugBase
+    export BUGBASE_PATH=~/miniconda2/envs/notebookServer/src/bugbase/BugBase
+    sudo apt-get install gfortran libblas-dev liblapack-dev
+    edit: ...envs/notebookServer/etc/conda/activate.d/env_vars.sh
+        #!/bin/bash
+        export PATH=...envs/notebookServer/src/bugbase/BugBase/bin:$PATH
+        export BUGBASE_PATH=...envs/notebookServer/src/bugbase/BugBase
+    ./bin/run.bugbase.r
+    """
+    def pre_execute(workdir, args):
+        if (type(args['counts'].index) == pd.core.indexes.numeric.Int64Index) \
+                or all(map(lambda x: x.isdigit(), args['counts'].index)):
+            args['counts'].to_csv('%s/input.txt' % workdir, sep="\t")
+        else:
+            raise ValueError(
+                ('Not all features are numerical, that might point to the fact'
+                 ' that your count table does not stem from closed reference '
+                 'picking vs. Greengenes.'))
+        if args['counts'].max().max() < 1:
+            raise ValueError('You need to insert frequencies, '
+                             'not relative abundances')
+
+    def commands(workdir, ppn, args):
+        commands = []
+
+        commands.append('run.bugbase.r -i %s/input.txt -a -o %s/results' %
+                        (workdir, workdir))
+
+        return commands
+
+    def post_execute(workdir, args):
+        normalized_counts = pd.read_csv(
+            '%s/results/normalized_otus/16s_normalized_otus.txt' % workdir,
+            sep="\t", index_col=0)
+        # normalize to abundances
+        normalized_counts /= normalized_counts.sum()
+        contrib_otus = pd.read_csv(
+            '%s/results/otu_contributions/contributing_otus.txt' % workdir,
+            sep="\t", index_col=0)
+
+        results = dict()
+        for trait in contrib_otus.columns:
+            results[trait] = pd.DataFrame(
+                (normalized_counts.values.T * contrib_otus[trait].values).T,
+                index=normalized_counts.index,
+                columns=normalized_counts.columns)
+
+        return results
+
+    return _executor('bugbase',
+                     {'counts': counts},
+                     pre_execute,
+                     commands,
+                     post_execute,
+                     ppn=1,
+                     environment='notebookServer',
+                     **executor_args)
+
+
 def correlation_diversity_metacolumns(metadata, categorial, alpha_diversities,
                                       beta_diversities,
                                       environment='qiime2-2018.2',
