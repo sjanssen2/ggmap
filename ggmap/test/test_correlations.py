7b7f3c13a41e1ca5e549adfad6f5ed44dcba0129
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-12-12 12:08:33 +0100

    fixing correlation test

--- ggmap/test/test_correlations.py
+++ ggmap/test/test_correlations.py
@@ -28,8 +28,11 @@ class CorrelationsTests(TestCase):
         with self.assertRaisesRegex(ValueError, 'The column\(s\) "agee", "kurt" is\/are not in the metadata\!'):
             correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={'agee': None}, intervals=['kurt'])
 
+        with self.assertRaisesRegex(ValueError, "You need to specify at least 2 columns!"):
+            correlate_metadata(self.metadata, categorials=[], ordinals={'age': ['AD', 'HY ?', 'IM']}, intervals=[])
+
         with self.assertRaisesRegex(ValueError, 'Not all values in column "age" can be interpreted as floats!'):
-            correlate_metadata(self.metadata, categorials=[], ordinals={}, intervals=['age'])
+            correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={}, intervals=['age'])
 
         with self.assertRaisesRegex(ValueError, '"ordinals" need to be a dictionary!'):
             correlate_metadata(self.metadata, categorials=[], ordinals={'age'}, intervals=[])
@@ -38,13 +41,10 @@ class CorrelationsTests(TestCase):
             correlate_metadata(self.metadata, categorials=[], ordinals={}, dates=[], intervals=[])
 
         with self.assertRaisesRegex(ValueError, 'Mapping for ordinal "age" must be either None or a list but not a "<class \'str\'>"!'):
-            correlate_metadata(self.metadata, categorials=[], ordinals={'age': 'AD'}, intervals=[])
-
-        with self.assertRaisesRegex(ValueError, "You need to specify at least two columns!"):
-            correlate_metadata(self.metadata, categorials=[], ordinals={'age': ['AD', 'HY ?', 'IM']}, intervals=[])
+            correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={'age': 'AD'}, intervals=[])
 
         with self.assertRaisesRegex(ValueError, "time data '05\/23\/1999' does not match format '\%d\/\%Y'"):
-            correlate_metadata(self.metadata, categorials=[], ordinals={}, dates={'collection_timestamp': "%d/%Y"}, intervals=[])
+            correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={}, dates={'collection_timestamp': "%d/%Y"}, intervals=[])
 
         e = StringIO()
         x = correlate_metadata(
@@ -54,12 +54,11 @@ class CorrelationsTests(TestCase):
             dates={'collection_timestamp': "%m/%d/%Y"},
             intervals=['latitude', 'longitude', 'weight', 'collection_second'],
             err=e)
-        self.assertEqual(e.getvalue(), 'Ordinal "age" does not specify label(s) "HY", "U".\n')
+        self.assertEqual(e.getvalue(), 'Ordinal "age" does not specify label(s) "HY", "HY ?", "IM", "U".\n')
 
-        # should be OK
-        # correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={}, intervals=[])
-        # correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={'age': None}, intervals=[])
-        # correlate_metadata(self.metadata, categorials=[], ordinals={}, intervals=['latitude'])
+        #should be OK
+        correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={'age': None}, intervals=[])
+        correlate_metadata(self.metadata, categorials=['bird_family'], ordinals={}, intervals=['latitude'])
 
 if __name__ == '__main__':
     main()
